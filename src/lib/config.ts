3f8360b82a625f724b5da480d883d60b5781f59d
Don <evidence@384.jp>
2023-11-17 11:08:25 +0900

    nostterのドメインを更新

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,5 +9,5 @@ export const relaysToGetRelays = [
 	'wss://nos.lol',
 	'wss://relay.damus.io',
 ];
-export const urlToLinkNote = 'https://nostter.vercel.app';
+export const urlToLinkNote = 'https://nostter.app';
 export const urlNIP07guide = 'https://scrapbox.io/nostr/nos2x%E3%81%AE%E3%82%BB%E3%83%83%E3%83%88%E3%82%A2%E3%83%83%E3%83%97%E3%81%A8%E4%BD%BF%E3%81%84%E6%96%B9';
